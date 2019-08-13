36e9ac481488992f1f744253de6f2a2cb0191473
Robert Jackson <rjackson@linkedin.com>
2019-08-13 10:19:33 -0400

    Fix issues when used within ember-engines.
    Avoiding reexport directly here because in some circumstances (ember-engines
    for example) a simple reexport is transformed to `define.alias`,
    unfortunately at the moment (ember-source@3.13) there is no _actual_
    `@ember/component` module to alias so this causes issues
    
    tldr; we can replace this with a simple reexport when we can rely on Ember
    actually providing a `@ember/component` module

--- app/components/-dynamic-element.js
+++ app/components/-dynamic-element.js
@@ -1,1 +1,10 @@
-export { default } from '@ember/component';
+import Component from '@ember/component';
+
+// avoiding reexport directly here because in some circumstances (ember-engines
+// for example) a simple reexport is transformed to `define.alias`,
+// unfortunately at the moment (ember-source@3.13) there is no _actual_
+// `@ember/component` module to alias so this causes issues
+//
+// tldr; we can replace this with a simple reexport when we can rely on Ember
+// actually providing a `@ember/component` module
+export default Component.extend();
