73400b27e519441bf0f8db885a54827f300042d2
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-07-24 11:37:33 +0200

    remove ember 3.24 from ember try

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -10,15 +10,6 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-resolver': '^8.0.0',
-            'ember-source': '~3.24.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
