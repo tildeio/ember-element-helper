ef8a3da47825e16dc9da0b96a63041d68c2900af
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-07-24 11:00:56 +0200

    Update test app v4.2.0...v4.12.0

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -4,16 +4,9 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
+  const app = new EmberApp(defaults, {
     // Add options here
   });
-
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
 
   if (app.env === 'test') {
     app.import('node_modules/ember-source/dist/ember-template-compiler.js', {
