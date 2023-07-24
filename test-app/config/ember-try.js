eca8dfc3c283904f3131f00ee266f17cf76c279a
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-07-24 11:25:50 +0200

    update lock file

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -10,17 +10,10 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.20',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.20.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
+            'ember-resolver': '^8.0.0',
             'ember-source': '~3.24.0',
           },
         },
@@ -29,6 +22,7 @@ module.exports = async function () {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
+            'ember-resolver': '^8.0.0',
             'ember-source': '~3.28.0',
           },
         },
@@ -37,6 +31,7 @@ module.exports = async function () {
         name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
+            'ember-resolver': '^8.0.0',
             'ember-source': '~4.4.0',
           },
         },
@@ -78,24 +73,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-          ember: {
-            edition: 'classic',
           },
         },
       },
